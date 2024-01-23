ba447c3c476827eebc8727154a65a12b73b8f59f
nahid <nahidcse.pub@gmail.com>
2024-01-24 03:23:14 +0600

    update database setting

--- backend/settings.py
+++ backend/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vote_app',
+        'USER': 'postgres',
+        'PASSWORD': 'nhbnhb',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
