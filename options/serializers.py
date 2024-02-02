b07173ef50484becfa3155b9f87b23b68421c71b
nahid <nahidcse.pub@gmail.com>
2024-02-02 19:29:15 +0600

    vote option percentages fix issue

--- options/serializers.py
+++ options/serializers.py
@@ -1,16 +1,9 @@
 from rest_framework import serializers
 
-from events.models import VoterSelectedOption
 from .models import Option
 
 
 class OptionSerializer(serializers.ModelSerializer):
-    option_total_votes = serializers.SerializerMethodField()
-
     class Meta:
         model = Option
-        fields = ['id', 'title', 'option_total_votes']
-
-    def get_option_total_votes(self, obj):
-        total_votes = VoterSelectedOption.objects.filter(selected_option_id=obj).count()
-        return total_votes
+        fields = ['id', 'title']
--- questions/serializers.py
+++ questions/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import serializers
 
 from events.models import VoterSelectedOption
@@ -8,11 +9,22 @@ from .models import Question
 class QuestionSerializer(serializers.ModelSerializer):
     options = OptionSerializer(many=True)
     question_total_votes = serializers.SerializerMethodField()
+    option_votes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Question
-        fields = ['id', 'title', 'event', 'question_total_votes', 'options']
+        fields = ['id', 'title', 'event', 'question_total_votes', 'option_votes_count', 'options']
 
     def get_question_total_votes(self, obj):
         total_votes = VoterSelectedOption.objects.filter(question_id=obj).count()
         return total_votes
+
+    def get_option_votes_count(self, obj):
+        option_counts = (
+            VoterSelectedOption.objects.filter(question_id=obj)
+            .values('selected_option')
+            .annotate(count=Count('selected_option'))
+        )
+
+        result = [{item['selected_option']: item['count']} for item in option_counts]
+        return result
